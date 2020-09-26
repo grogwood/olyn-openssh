bee81a5abb0f9c2a42285ef54d15751c6ea88c49
Scott Richardson <dev@grogwood.com>
2020-09-26 12:06:06 -0700

    Attribute renamed

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 # Load the openssh ports data bag item
-ssh_port_item = data_bag_item('ports', node[:olyn_openssh][:ports_data_bag_item])
+ssh_port_item = data_bag_item('ports', node[:olyn_openssh][:port][:data_bag_item])
 
 # Set the port to wrapper overrides
 node.override[:openssh][:server][:port] = ssh_port_item[:port]
